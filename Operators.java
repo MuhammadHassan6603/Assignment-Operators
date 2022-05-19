49470ce07d0253763baf035bb4d7c57b7751dde8
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-05-19 22:18:55 +0500

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators
+{
+ 	public static void main(String args[]) 
+	{
+		int x = 5;
+ 		int y = 3;
+ 		int z = 8;
+ 		x += 5;
+ 		y *= 4;
+ 		z += x * y;
+ 		System.out.println("x = " + x);
+ 		System.out.println("y = " + y);
+ 		System.out.println("z = " + z);
+ 	}
+}
